8185a4edc8dc9cc032cebdf284ace6dd850642a8
Aathraey S <129571847+astray-s@users.noreply.github.com>
2024-03-17 10:19:10 -0700

    Update gen_data.py

--- gen_data.py
+++ gen_data.py
@@ -1,27 +1,25 @@
+"""
+
+import csv
+import random
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/generate_csv')
+def generate_csv():
+  zipcodes = [random.randint(10000, 99999) for i in range(100)]
+  consumption = [random.randint(10, 100) for i in range(100)]
+  
+  with open('consumption.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['zipcode', 'average_consumption'])
+    for z, c in zip(zipcodes, consumption):
+    writer.writerow([z, c])
+    
+    return 'CSV file generated successfully!'
 
 
-# import csv
-# import random
+app.run(debug=True)
 
-# from flask import Flask
-# app = Flask(__name__)
-
-# @app.route('/generate_csv')
-# def generate_csv():
-#     # Generate random zipcodes
-#     zipcodes = [random.randint(10000, 99999) for i in range(100)]
-
-#     # Generate random water consumption values 
-#     consumption = [random.randint(10, 100) for i in range(100)]
-
-#     # Write data to CSV file
-#     with open('consumption.csv', 'w', newline='') as f:
-#         writer = csv.writer(f)
-#         # writer.writerow(['zipcode', 'average_consumption'])
-#         for z, c in zip(zipcodes, consumption):
-#             writer.writerow([z, c])
-
-#     return 'CSV file generated successfully!'
-
-
-# app.run(debug=True)
+"""
